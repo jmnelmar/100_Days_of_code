a28192e41c2fbe79089c2b3cb904f1e35fa86eb5
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-10 10:04:15 -0800

    Adding random walk project

---
+++ 100_Days_of_code/Python/Day-18-4/README.md
@@ -0,0 +1,13 @@
+# Drawing random paths
+
+Using turtle grpahics to draw diferent shapes in pyton
+
+## Topics
+1. [random](https://docs.python.org/3/library/random.html)
+2. [turtle](https://docs.python.org/3/library/turtle.html#module-turtle)
+3. [Random walk](https://en.wikipedia.org/wiki/Random_walk)
+
+## how to use.
+```python
+python3 day_18_4.py
+```
---
+++ 100_Days_of_code/Python/Day-18-4/day_18_4.py
@@ -0,0 +1,19 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+########### Challenge 4 - Random Walk ########
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+def change_color(tim):
+    R = random.random()
+    B = random.random()
+    G = random.random()
+    tim.color(R, G, B)
+
+for i in range (1,100):
+  angle = random.randint(1,4) * 90
+  tim.left(angle)
+  tim.color( colours[random.randint(0,7)] )
+  #change_color(tim)
+  tim.forward(30)
