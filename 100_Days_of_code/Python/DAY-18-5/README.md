505e66af5d7f6a9fceca3afd2dee346ec7d01bff
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-10 12:45:03 -0800

    Adding project ady 18-5 Sephirograph with turtle grpahics

---
+++ 100_Days_of_code/Python/DAY-18-5/README.md
@@ -0,0 +1,11 @@
+# Day 18-5 Spirograph
+
+## Topics
+1. [random](https://docs.python.org/3/library/random.html)
+2. [turtle](https://docs.python.org/3/library/turtle.html#module-turtle)
+3. [Random walk](https://en.wikipedia.org/wiki/Random_walk)
+
+## how to use.
+```python
+python3 day_18_5.py
+```
---
+++ 100_Days_of_code/Python/DAY-18-5/day_18_5.py
@@ -0,0 +1,19 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########### Challenge 5 - Spirograph ########
+
+for i in range(1, 361, 10):
+  tim.color(random_color())
+  tim.circle(100)  
+  tim.left(i)
+
