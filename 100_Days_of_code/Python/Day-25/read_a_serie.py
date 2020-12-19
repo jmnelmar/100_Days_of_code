ec4994ded0d3ddb138f649d0e4d08ed5f1862c1b
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-19 00:38:15 -0800

    get a serie script

--- 100_Days_of_code/Python/Day-25/read_a_serie.py
+++ 100_Days_of_code/Python/Day-25/read_a_serie.py
@@ -1,4 +1,12 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+def get_serie(path, serie_name):
+    data = pandas.read_csv(path)
+    return data[serie_name]
+
+
+path = "weather_data.csv"
+serie_name = "temp"
+
+serie = get_serie(path, serie_name)
+print(serie)
