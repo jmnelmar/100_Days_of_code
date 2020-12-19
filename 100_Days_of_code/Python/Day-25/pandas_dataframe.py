c4792c20a9d9c3cfaace3bcda8afe18b568a8ecb
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-19 01:19:33 -0800

    dataframes

---
+++ 100_Days_of_code/Python/Day-25/pandas_dataframe.py
@@ -0,0 +1,14 @@
+#Create Data Frame from scratch
+
+
+
+import pandas
+
+data_dict = {
+    "students":["Amy", "James", "Angela"],
+    "scores":[76,56,65]
+}
+
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
