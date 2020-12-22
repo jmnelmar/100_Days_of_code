48f534c3b920cbff0a6b064b8cce6cea3f7c1440
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-21 23:34:13 -0800

    spliting the script in clase board and gamestate script

--- 100_Days_of_code/Python/Day-25/GameState/gamestate.py
+++ 100_Days_of_code/Python/Day-25/GameState/gamestate.py
@@ -7,20 +7,13 @@ FONT = ("Courier", 12, "normal")
 ALIGMENT = "left"
 LEVEL_POSITION = (-280,280)
 
-#screen = turtle.Screen()
-#screen.title("U.S States Game")
-##image = "blank_states_img.gif"
-#screen.addshape(image)
-#turtle.shape(image)
 answer = ""
 br = Board()
-
 
 def get_mouse_click_coor(x,y):
     answer = br.screen.textinput(title=f"Guess the State{br.points}/50", prompt="What's another state's name?")
     data = pandas.read_csv("50_states.csv")
-    serie = data[ data.state == answer ]   
-    print(serie) 
+    serie = data[ data.state.str.lower() == answer.lower() ]   
     if not serie.empty:
         points = br.points +1
         corx =  int(serie.x) 
@@ -32,10 +25,3 @@ def get_mouse_click_coor(x,y):
            
 turtle.onscreenclick(get_mouse_click_coor)
 turtle.mainloop()
-
-
-#screen.exitonclick()
-
-#-327.0 114.0, -282.0 100.0
-
-#-278.0 -56.0, -242.0 -45.0, 
