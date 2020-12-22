2cbce02fc042882099dfcf6bd6fbe08370c27bef
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-12-21 22:23:44 -0800

    adding day-25

---
+++ 100_Days_of_code/Python/Day-25/state_projects.py
@@ -0,0 +1,60 @@
+import turtle
+import pandas
+from pandas.core.indexing import convert_to_index_sliceable
+
+FONT = ("Courier", 12, "normal")
+ALIGMENT = "left"
+LEVEL_POSITION = (-280,280)
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+answer = ""
+points = 0
+
+
+
+def print_name(x,y,state):
+    pen = turtle.Turtle()
+    pen.color("black")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(x,y)
+    pen.write(state)
+
+
+def get_mouse_click_coor(x,y):
+    answer = screen.textinput(title=f"Guess the State{points}/50", prompt="What's another state's name?")
+    #data = pandas.read_csv("50_states.csv")
+    #serie = data[ data.state == answer ]
+    #print("state: "+serie["state"]+", x: "+str(serie["x"]) +", y: "+str(serie["y"])) 
+
+    data = pandas.read_csv("50_states.csv")
+    serie = data[ str(data.state).lower() == answer.lower() ]
+    
+    
+    if not serie.empty:
+        corx =  int(serie.x) 
+        cory = int(serie.y)
+        state = serie.values[0][0]
+       
+        points = get_points(points,1) 
+        print_name(corx,cory,state)
+       
+    else:
+        print("not ok")
+
+
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
+
+
+#screen.exitonclick()
+
+#-327.0 114.0, -282.0 100.0
+
+#-278.0 -56.0, -242.0 -45.0, 
