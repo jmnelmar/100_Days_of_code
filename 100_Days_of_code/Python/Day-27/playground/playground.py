2c744f891191da22c5b08f2ee45deafac0ed10bd
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-03 13:04:34 -0800

    kwargs

--- 100_Days_of_code/Python/Day-27/playground/playground.py
+++ 100_Days_of_code/Python/Day-27/playground/playground.py
@@ -5,3 +5,13 @@ def add(*args):
     return response
 
 print(add(1,2,3,4,5,6,100))
+
+def calculate(**kwargs):
+    print(kwargs)
+
+    for key,value in kwargs.items():
+        print(key)
+        print(value)
+
+
+calculate(add=3, multiply=5)
