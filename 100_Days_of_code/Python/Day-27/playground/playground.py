fdb023613aac6e97d0a1bc1ac663b5a712095995
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-03 13:15:14 -0800

    class car using kwargs and method get

--- 100_Days_of_code/Python/Day-27/playground/playground.py
+++ 100_Days_of_code/Python/Day-27/playground/playground.py
@@ -6,12 +6,28 @@ def add(*args):
 
 print(add(1,2,3,4,5,6,100))
 
-def calculate(**kwargs):
+def calculate(n,**kwargs):
     print(kwargs)
 
-    for key,value in kwargs.items():
-        print(key)
-        print(value)
+    #for key,value in kwargs.items():
+    #    print(key)
+    #    print(value)
+    #print(kwargs["add"])
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2,add=3, multiply=5)
 
 
-calculate(add=3, multiply=5)
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw.get("model")#si no existe devuelve None
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+
+my_car = Car(make="Nissan")
+print(my_car.model)
