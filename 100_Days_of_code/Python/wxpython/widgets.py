9af3ce08f06d4146142fcb7c0d570e9c635a58e9
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-05 20:11:57 -0800

    widgets

---
+++ 100_Days_of_code/Python/wxpython/widgets.py
@@ -0,0 +1,24 @@
+import wx
+from wx.core import Size
+
+class MyApp(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(400,400))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.ok_btn = wx.Button(self, -1, label="OK")
+        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer2.Add(self.ok_btn, 1, wx.EXPAND)
+
+        # Use some sizers to see layout options
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.control, 1, wx.EXPAND)
+        self.sizer.Add(self.sizer2, 0, wx.EXPAND)
+
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        #self.sizer.Fit(self)
+        self.Show()
+
+app = wx.App()
+frame = MyApp(None,"My app")
+app.MainLoop()
