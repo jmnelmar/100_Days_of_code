8853e4f22048606fafc832f82c22aec9551e49cf
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-08 00:23:13 -0800

    adding exceptions

--- 100_Days_of_code/Python/Day-33/api_endpoints.py
+++ 100_Days_of_code/Python/Day-33/api_endpoints.py
@@ -2,4 +2,9 @@ import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-print(response)
+print(response.status_code)
+
+if response.status_code == 404:
+    raise Exception("That resource doesnt exist")
+elif response.status_code == 401:
+    raise Exception("You are not authorised to access this")
