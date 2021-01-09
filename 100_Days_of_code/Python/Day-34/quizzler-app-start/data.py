6e2c44df3fd5d9b4928fca36ab97fb24efb7cc78
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-09 01:05:00 -0800

    adding quiz ui project

---
+++ 100_Days_of_code/Python/Day-34/quizzler-app-start/data.py
@@ -0,0 +1,9 @@
+import requests
+params = {
+    "amount":10,
+    "type":"boolean"
+}
+response = requests.get("https://opentdb.com/api.php", params)
+response.raise_for_status()
+question_data = response.json()["results"]
+
