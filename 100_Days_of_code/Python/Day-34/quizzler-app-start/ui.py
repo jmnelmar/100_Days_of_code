fbf6ce767457ee5d6deee91534dd04e42006dc36
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-11 00:20:32 -0800

    adding score logic, background and end of the game logic

--- 100_Days_of_code/Python/Day-34/quizzler-app-start/ui.py
+++ 100_Days_of_code/Python/Day-34/quizzler-app-start/ui.py
@@ -3,6 +3,7 @@ THEME_COLOR = "#375362"
 from tkinter import *
 from quiz_brain import QuizBrain
 
+
 class QuizInterface:
 
     def __init__(self, quiz_brain: QuizBrain):
@@ -10,7 +11,7 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizler")
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
-
+        self.is_game_end = False
 
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
@@ -42,15 +43,34 @@ class QuizInterface:
         self.score_label.config(text=f"Score: {self.quiz.score}")
 
     def get_next_question(self):
+        q_text=""
         if self.quiz.still_has_questions():
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
             self.get_score()
+        else:
+            self.get_score()
+            q_text = f"Game over\n Final Score {self.quiz.score}/10"
+            self.canvas.itemconfig(self.question_text, text=q_text)
+            self.is_game_end = True
+        self.set_bg_white()
 
     def true_pressed(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        self.answer_bg(self.quiz.check_answer("True"))
+        
 
     def false_pressed(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        self.answer_bg(self.quiz.check_answer("False"))
+        
+
+    def answer_bg(self, answer:bool):
+        if self.is_game_end == False:
+            if answer:
+                self.canvas.config(bg="green")
+            else:
+                self.canvas.config(bg="red")
+            self.window.after(1000, self.get_next_question)
+        
+    def set_bg_white(self):
+        self.canvas.config(bg="white")
+        
