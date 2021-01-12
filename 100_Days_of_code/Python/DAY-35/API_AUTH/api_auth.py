61eccce771879ea436bcfda8bf7ab3d687d8634d
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-11 21:40:05 -0800

    Adding api auth scripts

---
+++ 100_Days_of_code/Python/DAY-35/API_AUTH/api_auth.py
@@ -0,0 +1,17 @@
+import requests
+#https://home.openweathermap.org/myservices
+# jmnel_mar 
+
+api_key = "8682a293fbd0d9c0c576f644784d429a"
+
+
+members ={
+    "lat":33.441792,
+    "lon":-94.037689,
+    "exclude":"current,minutely,daily,alerts",
+    "appid":"8682a293fbd0d9c0c576f644784d429a"
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/onecall",members)
+data = response.json()["hourly"]
+print(data)
