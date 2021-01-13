b840efc4d1814bd0b3cdbf75768f7b7ad0cb650d
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-01-12 21:33:07 -0800

    adding suplementary scripts

---
+++ 100_Days_of_code/Python/DAY-35/rain_warning_sms/set_env.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+TWILIO_ACCOUNT_SID='put account here'
+TWILIO_AUTH_TOKEN='put the token here'
+export TWILIO_ACCOUNT_SID
+echo 'TWILIO_ACCOUNT_SID set.'
+export TWILIO_AUTH_TOKEN
+echo 'TWILIO_AUTH_TOKEN set'
